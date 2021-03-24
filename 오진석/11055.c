7b30f6a6db81e6949b15f8d4d4f0fa1324666a97
jinseok3121 <73455515+jinseok3121@users.noreply.github.com>
2021-03-24 21:25:09 +0900

    Update 11055.c

--- 오진석/11055.c
+++ 오진석/11055.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 
+// 현재의 값이 이전의 값보다 크면 증가 부분 수열 
+// 현재 위치까지의 모든 증가 부분 수열을 배열 dp에 저장
+
 int main()
 {
-    int A[1001], DP[1001], N, max = 0; //A : 수열 배열, DP : 현재 위치까지의 증가 부분 수열 배열
+    int A[1001], DP[1001], N, max = 0; // A : 입력할 수열 배열, DP : 현재 위치까지의 증가 부분 수열 배열
                                        // N : A의 크기, max : 최댓값
     int i,j;
-    scanf("%d", &N);
+    scanf("%d", &N); // N값 입력
     
     for(int i = 0; i<N; i++)
     {
-        scanf("%d", &A[i]);
+        scanf("%d", &A[i]); // N개만큼 수열 입력
     }
     
     for(i = 0; i<N; i++)
     {
-        DP[i] = A[i];
+        DP[i] = A[i]; //DP배열에 A배열값 넣기 
         
         for(j = 0; j < i; j++)
         {
@@ -25,23 +28,11 @@ int main()
         }
         if(max < DP[i])
         {
-            max = DP[i];
+            max = DP[i]; // 최댓값 갱신
         }
         
     }
     printf("%d", max);
-}
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
     return 0;
 }
